0578169965f003f3f7ef5317caf83ecb943988fd
rlaehghks5 <rlaehghks5@gmail.com>
2024-02-29 06:47:42 +0000

    remove model.eval() in main.py

--- main.py
+++ main.py
@@ -24,16 +24,14 @@ peft_model_id = 'DoHwan9672/SOLAR10.7B-Divorce-Lawyers-LLM-PEFT'
 model = PeftModel.from_pretrained(model, peft_model_id).to('cuda', non_blocking=True)
 
 print('load done')
-model.eval()
-
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-prompter = Prompter("./templates/kullm")
+prompter = Prompter("kullm")
 
 def infer(instruction="", input_text=""):
     prompt = prompter.generate_prompt(instruction, input_text)
     print('prompt:\n', prompt)
-    output = pipe(prompt, max_length=1024, temperature=0.8, do_sample=True, eos_token_id=32000, top_p=0.8, top_k=70, truncation=True)
+    output = pipe(prompt, max_length=1024, temperature=0.8, do_sample=True, eos_token_id=32000, top_p=0.7, top_k=80, truncation=True)
     s = output[0]["generated_text"]
     result = prompter.get_response(s)
 
